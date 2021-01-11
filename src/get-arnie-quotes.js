46e32bf3f30a71b3b3881908b169375648766f60
Stewart urica <user.name>
2021-01-11 23:09:00 +1100

    Get test cases passed.

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,19 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // To handle Arnie Quotes data
+  return Promise.all(
+    urls.map(url => httpGet(url))
+  ).then(results => {
+    // Resolve all responses from urls
+    return results.map(result => {
+      const body = JSON.parse(result.body);
+      // Return Arnie Quote message when the quote is found, otherwise return error message
+      return result.status === 200 ? {"Arnie Quote": body.message} : {"FAILURE": body.message};
+    });
+  }).catch((e) => {
+    // Handle error
+  });
 };
 
 module.exports = {
